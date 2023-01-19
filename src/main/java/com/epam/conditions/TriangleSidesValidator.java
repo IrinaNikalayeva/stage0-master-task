414ca47fde531041d768e9229b31e09d196b54ca
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-19 22:39:20 +0100

    conditions tasks

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,7 +3,15 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        boolean condition1 = (firstSide + secondSide) > thirdSide;
+        boolean condition2 = (secondSide + thirdSide) > firstSide;
+        boolean condition3 = (firstSide + thirdSide) > secondSide;
+        if ((condition1 && condition2) && condition3) {
+            System.out.println("this is a valid triangle");
+        } else {
 
+            System.out.println("it's not a triangle");
+        }
     }
 
 }
