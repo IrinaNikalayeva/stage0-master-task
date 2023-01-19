42628c04db035c7769767f1d820a3ff72ccf1c4a
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-19 22:41:44 +0100

    loops tasks

--- src/main/java/com/epam/loops/Power.java
+++ src/main/java/com/epam/loops/Power.java
@@ -3,6 +3,12 @@ package com.epam.loops;
 public class Power {
 
     public void printPower(int numberToPrint, int power) {
+        int finalNum = 1;
+        for (int i = 1; i <= power; i++) {
+            finalNum *= numberToPrint;
+        }
+        System.out.println(finalNum);
+
 
     }
 
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,19 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
 
+            for (int j = i; j < cathetusLength; j++) {
+                System.out.print(' ');
+            }
+
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print(Math.abs(i - j) + 1);
+            }
+
+            System.out.println();
+
+        }
     }
 
     public static void main(String[] args) {
